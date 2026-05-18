cc05c3c55fc048b1805c1c8ff78f442f48d8b97a
faharAhnaf <faharahnaf66@gmail.com>
2026-05-18 11:11:02 +0700

    chore: enable static export output in Next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+	output: "export",
+
 	experimental: {
 		optimizePackageImports: ["lucide-react", "@radix-ui/react-icons"],
 	},
