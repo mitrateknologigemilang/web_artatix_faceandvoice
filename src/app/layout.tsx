55ba929648a7a165aab157cf6235c20a4bd1809a
faharAhnaf <faharahnaf66@gmail.com>
2026-05-29 18:03:31 +0700

    feat: add favicon, web manifest, and update app metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,24 +6,32 @@ import { SidebarConfigProvider } from "@/contexts/sidebar-context";
 import { inter } from "@/lib/fonts";
 
 export const metadata: Metadata = {
-  title: "Shadcn Dashboard",
-  description: "A dashboard built with Next.js and shadcn/ui",
+	title: "Artatix Jomlo Festival 2026",
+	description:
+		"Platform verifikasi wajah dan suara untuk Artatix Jomlo Festival 2026",
+	icons: {
+		icon: [
+			{ url: "/favicon-96x96.png", type: "image/png", sizes: "96x96" },
+			{ url: "/favicon.svg", type: "image/svg+xml" },
+		],
+		shortcut: "/favicon.ico",
+		apple: [{ url: "/apple-touch-icon.png", sizes: "180x180" }],
+	},
+	manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en" className={`${inter.variable} antialiased`}>
-      <body className={inter.className}>
-        <ThemeProvider defaultTheme="system" storageKey="nextjs-ui-theme">
-          <SidebarConfigProvider>
-            {children}
-          </SidebarConfigProvider>
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" className={`${inter.variable} antialiased`}>
+			<body className={inter.className}>
+				<ThemeProvider defaultTheme="system" storageKey="nextjs-ui-theme">
+					<SidebarConfigProvider>{children}</SidebarConfigProvider>
+				</ThemeProvider>
+			</body>
+		</html>
+	);
 }
